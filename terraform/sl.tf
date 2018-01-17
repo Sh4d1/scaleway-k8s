714af05e38c8847b83004e0c60ee37f7da103520
Patrik Cyvoct <patrik.cyvoct@gmail.com>
2018-01-17 14:23:31 +0100

    update to k8s 1.9.1, and change to debian stretch

--- terraform/sl.tf
+++ terraform/sl.tf
@@ -2,55 +2,48 @@ provider "scaleway" {
   region = "${var.region}"
 }
 
-data "scaleway_image" "ubuntu" {
+data "scaleway_image" "os" {
   architecture = "${var.architecture}"
   name         = "${var.image}"
 }
 
-resource "scaleway_ip" "public_ip" {
-  count = 1
-}
+//resource "scaleway_ip" "public_ip" {
+//  count = 1
+//}
 
 resource "scaleway_server" "worker" {
   count  = "${var.worker_instance_count}"
   name   = "worker${count.index}"
-  image  = "${data.scaleway_image.ubuntu.id}"
+  image  = "${data.scaleway_image.os.id}"
   type   = "${var.worker_instance_type}"
   state  = "running"
   tags   = ["workers"]
-
-  volume {
-    size_in_gb = 50
-    type       = "l_ssd"
-  }
 }
+
+//  volume {
+//    size_in_gb = 25
+//    type       = "l_ssd"
+//}
+
 
 resource "scaleway_server" "master" {
   count  = "${var.master_instance_count}"
   name   = "master${count.index}"
-  image  = "${data.scaleway_image.ubuntu.id}"
+  image  = "${data.scaleway_image.os.id}"
   type   = "${var.master_instance_type}"
   state  = "running"
   tags   = ["masters"]
 }
 
-resource "scaleway_server" "proxy0" {
-  count = 1
-  name  = "proxy0"
-  image = "${data.scaleway_image.ubuntu.id}"
+resource "scaleway_server" "proxy" {
+  count = "${var.proxy_instance_count}"
+  name  = "proxy${count.index}"
+  image = "${data.scaleway_image.os.id}"
   type  = "${var.proxy_instance_type}"
-  public_ip  = "${element(scaleway_ip.public_ip.*.ip, count.index)}"
+  //public_ip  = "${count.index == "0" ? element(scaleway_ip.public_ip.*.ip, count.index) : "0"}"
+  public_ip = "51.15.217.165"
   state  = "running"
-  tags  = ["proxy","primary"]
-}
-
-resource "scaleway_server" "proxy1" {
-  count = 1
-  name  = "proxy1"
-  image = "${data.scaleway_image.ubuntu.id}"
-  type  = "${var.proxy_instance_type}"
-  state  = "running"
-  tags  = ["proxy","secondary"]
+  tags  = ["proxy","${count.index == "0" ? "primary" : "secondary"}"]
 }
 
 output "worker_private_ips" {
@@ -61,14 +54,10 @@ output "master_private_ips" {
   value = ["${scaleway_server.master.*.private_ip}"]
 }
 
-output "proxy0_private_ips" {
-  value = ["${scaleway_server.proxy0.*.private_ip}"]
-}
-
-output "proxy1_private_ips" {
-  value = ["${scaleway_server.proxy1.*.private_ip}"]
+output "proxy_private_ips" {
+  value = ["${scaleway_server.proxy.*.private_ip}"]
 }
 
 output "public_ip" {
-  value = ["${scaleway_server.proxy0.*.public_ip}"]
+  value = ["${scaleway_server.proxy.*.public_ip}"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,33 +3,37 @@ variable "region" {
 }
 
 variable "architecture" {
-  default = "x86_64"
+  default = "arm"
 }
 
 variable "image" {
-  default = "Ubuntu Zesty"
+  default = "Debian Stretch"
 }
 
 variable "master_instance_type" {
-  default = "VC1S"
+  default = "C1"
 }
 
 variable "master_instance_count" {
-  default = 3
+  default = 1
 }
 
 variable "proxy_instance_type" {
-  default = "VC1S"
+  default = "C1"
+}
+
+variable "proxy_instance_count" {
+  default = 1
 }
 
 variable "worker_instance_type" {
-  default = "VC1M"
+  default = "C1"
 }
 
 variable "worker_volume_size" {
-  default = 100
+  default = 50
 }
 
 variable "worker_instance_count" {
-  default = 3
+  default = 2 
 }
